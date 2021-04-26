da14f7e91bcd9da72b2323efba29d4324f70f607
Aravind Narayanan <76870716+aiandds@users.noreply.github.com>
2021-04-26 12:37:08 +0530

    Update ex1.py

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,71 +1,83 @@
-#enna vaynumo atha import pannanum
+#importing necessary library
 import csv
 import statistics
 import numpy
-#variable create pannanum 
+# creating variables and list 
 li=[]
-ans=0
+ans = 0
 count = 0
 
-#file open panrom
+# open file 
 with open("car.csv", "r") as file:
 
     car = csv.DictReader(file)
-    #first row ahh print pandrom
+    
     for row in car:
+        # print first row 
         print(row["Car"])
         li.append(row["Horsepower"])
-        # 3 column ahh add pandrom
+        # adding 3 column 
         ans += float(row["MPG"]) + float(row["Cylinders"]) + float(row["Horsepower"])
-        # mothamaa yathuna row eruku 
+        # to find tot num of row
         count += 1
-# print print
+# total of 3 column
 print("Total of three columns 'MPG','Cylinders' and 'Horsepower' = ",ans)
-
+#print total number of row 
 print("Number of rows in a dataset = ",count)
 
-# string ahh int ahh convert pandrom in a 'list'
+# converting string into int in the list 
 for i in range(0, len(li)):
     li[i] = int(li[i])
-# max and min ahh kandopudikom
+# find max and min 
 maxx = max(li)
 minn = min(li)
 print("Maximum element in a column = ",maxx)
 print("Minimum element in a column = ",minn)
 
-# ethuku approm ella statistics function um pandrom
+# calculate necessary statistics value and print the value 
 meann = statistics.mean(li)
 print("Mean = ",meann)
+
 med = statistics.median(li)
 print("Median = ", med)
+
 modd = statistics.mode(li)
 print("Mode = ",modd)
+
 fm = statistics.fmean(li)
 print("fmean = ",fm)
+
 hm = statistics.harmonic_mean(li)
 print("Harmonic mean = ",hm)
+
 gm = statistics.geometric_mean(li)
 print("Geometric mean = ", gm)
+
 ml = statistics.median_low(li)
 print("Median low = ",ml)
+
 mh = statistics.median_high(li)
 print("Median high = ",mh)
+
 pt = numpy.percentile(li,100)
 print("Percentile = ",pt)
+
 qt = numpy.quantile(li, .25)
 print("Quantile = ",qt)
+
 nanme = numpy.nanmean(li)
 print("nanmean = ",nanme)
+
 sd = statistics.stdev(li)
 print("Standard Deviation = ",sd)
+
 vary = statistics.variance(li)
 print("Variance = ",vary)
 
-
-# ethu 19 and 20 task 
-# multimode and second mode kandopudikom 
+# multimode and second mode in the column
 own1 = statistics.multimode(li)
 print("Multimode = ",own1)
+
 rmod = statistics.mode(li)
 
 while (li.count(rmod)):
